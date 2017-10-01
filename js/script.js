6395d138b503e01228d1a82a68eb864aba05e572
MasseyIsaako <lasiisaako@gmail.com>
2017-10-01 23:39:25 +1300

    added initial flex capability and default navbar
    just experimenting with centering using flexbox css methodology, seems to be working quite well. I may continue using it in the future

--- js/script.js
+++ js/script.js
@@ -8,41 +8,36 @@ var mobileIcon = $("#mobile-menu");
 var slide = $(".slide");
 
 $(document).ready(function(){
+	// Setting slide width based on window - navContainer width.
+	slide.css("width", ($(window).width() - navContainer.width()));
 
 	// Mobile Navigation Interactivity
 	navContainer.click(function(){
-		var originalWidth = navContainer.width;
-		console.log($(window).width());
+			if(!mobileNav){
+				navContainer.css("width", "50px");
+				slide.css("margin-left", "0px");
+				$(".nav-item").fadeOut(500);
 
-		if(!mobileNav && ( $(window).width() < 1199 ) ){
-			navContainer.css("width", originalWidth);
-			slide.css("margin-left", "0px");
-			$(".nav-item").fadeOut(1000);
+				// Changing the Chevron based on whether nav is open or not
+				mobileIcon.removeClass("fa-chevron-left");
+				mobileIcon.addClass("fa-chevron-right");
+				
+				mobileNav = true;
+			} else {
+				navContainer.css("width", "100vw");
+				$(".nav-item").fadeIn(500);
+				console.log(navContainer[0].style.width);
 
-			// Changing the Chevron based on whether nav is open or not
-			mobileIcon.removeClass("fa-chevron-left");
-			mobileIcon.addClass("fa-chevron-right");
-			
-			setTimeout(function(){
-				mobileIcon.css("text-align", "center");
-			}, 1000);
-			
-			mobileNav = true;
-		} else{
-			navContainer.css("width", "150px");
-			slide.css("margin-left", "150px");
-			$(".nav-item").fadeIn(1000);
-
-			// Changing the Chevron based on whether nav is open or not
-			mobileIcon.removeClass("fa-chevron-right");
-			mobileIcon.addClass("fa-chevron-left");
-			mobileIcon.css("text-align", "right");
-			
-			mobileNav = false;
-		}
+				// Changing the Chevron based on whether nav is open or not
+				mobileIcon.removeClass("fa-chevron-right");
+				mobileIcon.addClass("fa-chevron-left");
+				
+				mobileNav = false;
+			}
 	});
 });
 
-setTimeout(function(){
-	$('#tip').css('display','none');
-},800);
+// Responsive resize of slide based on window
+$(window).resize(function(){
+	slide.css("width", ($(window).width() - navContainer.width()));
+});
